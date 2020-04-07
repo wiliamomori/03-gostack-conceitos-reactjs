cd340a0af53c27ba89c94459d7df5ed8d0ecf5cd
Wiliam Omori <wiliam.omori@gmail.com>
2020-04-07 09:26:34 -0300

    Desafio 03: Conceitos do ReactJS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from './services/api';
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [ repositories, setRepositories ]  = useState([]);
+
+  useEffect( () => {
+    axios.get('repositories').then( response => {
+      setRepositories(response.data);
+    } );
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    try{
+      const repository = {
+        title: `Repositório ${Date.now()}`,
+      };
+
+      const response = await axios.post('repositories', repository);
+      setRepositories([ ...repositories, response.data ]);
+    } catch(e){
+      alert('Erro ao adicionar um repositório');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try{
+      const _repositories = repositories;
+      
+      await api.delete(`repositories/${id}`);
+
+      const repositoryIndex = _repositories.findIndex( repository => repository.id === id );
+
+      _repositories.splice(repositoryIndex, 1);
+
+      setRepositories([ ...repositories ]);
+
+    } catch(e){
+      alert('Erro ao deletar o repositório');
+    }
   }
+
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map( (repository, i) => { return (
+          <li key={i}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )} )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
